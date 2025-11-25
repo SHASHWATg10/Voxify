b171efef78cb518a20e5c4466148b4d625678673
Shashwat Gupta <shashwat.gupta.sg10@gmail.com>
2025-11-25 19:53:43 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/voxify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.voxify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:voxify/home_page.dart';
+import 'package:voxify/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Voxify',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+
+      home: const HomePage(),
+    );
+  }
+}
